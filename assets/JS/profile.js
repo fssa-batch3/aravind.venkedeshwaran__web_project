910b4a6de392f90b1a53b4ac05ef0041adc8bba3
aravindramvjs <aravindramvj@gmail.com>
2023-04-07 12:30:20 +0530

    fix: google sign in redirection

--- assets/JS/profile.js
+++ assets/JS/profile.js
@@ -1,57 +1,57 @@
-// let users = JSON.parse(localStorage.getItem('userdata'));
-// let userlogin = JSON.parse(localStorage.getItem('userlogin'));
+let users = JSON.parse(localStorage.getItem('userdata'));
+let userlogin = JSON.parse(localStorage.getItem('userlogin'));
 
-// for (let i = 0; i <  users.length; i++) {
-//     if(userlogin == users[i].email  ||userlogin == users[i].name ){
-//     document.getElementById("name").value = users[i]["name"];
-//     document.getElementById("email").value = users[i]["email"];
-//     }
-// }
+for (let i = 0; i <  users.length; i++) {
+    if(userlogin == users[i].email  ||userlogin == users[i].name ){
+    document.getElementById("name").value = users[i]["name"];
+    document.getElementById("email").value = users[i]["email"];
+    }
+}
 //  enable and disable input for edit and update operation
-// function enablename() {
-// document.getElementById("name").disabled = false;
-// }
-// function enablepassword() {
-//     document.getElementById("password").disabled = false;
-// }
+function enablename() {
+document.getElementById("name").disabled = false;
+}
+function enablepassword() {
+    document.getElementById("password").disabled = false;
+}
 
 
-// // Edit User Profile
-// function editprofile() {
-//     for(let i = 0 ; i < users.length; i++){
-//     if(document.getElementById("email").value == users[i]["email"]){
-//         users[i].name = document.getElementById("name").value;
-//         users[i].password = document.getElementById("password").value;
-//         }
-//     }
+// Edit User Profile
+function editprofile() {
+    for(let i = 0 ; i < users.length; i++){
+    if(document.getElementById("email").value == users[i]["email"]){
+        users[i].name = document.getElementById("name").value;
+        users[i].password = document.getElementById("password").value;
+        }
+    }
 
-//     if(document.getElementById("name").value != "" && document.getElementById("password").value != ""){
-//     localStorage.setItem('userdata',JSON.stringify(users))
-//     Notify.success("Successfully your profile updated")
-//     // alert("Successfully your profile updated")
-//     }
-//     else{
-//         // alert("Username or password field could not be empty")
-//         Notify.error("Username or password field could not be empty")
-//     }
-// }
+    if(document.getElementById("name").value != "" && document.getElementById("password").value != ""){
+    localStorage.setItem('userdata',JSON.stringify(users))
+    Notify.success("Successfully your profile updated")
+    // alert("Successfully your profile updated")
+    }
+    else{
+        // alert("Username or password field could not be empty")
+        Notify.error("Username or password field could not be empty")
+    }
+}
 
 
-// // Delete User Profile
-// function deleteprofile() {
-//     confirm("Are you sure want to delete your account?")
+// Delete User Profile
+function deleteprofile() {
+    confirm("Are you sure want to delete your account?")
 
-//     for (let i = 0; i <  users.length; i++) {
+    for (let i = 0; i <  users.length; i++) {
 
-//     if(document.getElementById("email").value == users[i]["email"]){
-//         users.splice(i,1)//(2,1)
-//         localStorage.setItem("userdata", JSON.stringify(users));
-//         window.location.href = 'signup.html';
-//         Notify.error("Profile Deleted")
-//     }
+    if(document.getElementById("email").value == users[i]["email"]){
+        users.splice(i,1)//(2,1)
+        localStorage.setItem("userdata", JSON.stringify(users));
+        window.location.href = 'signup.html';
+        Notify.error("Profile Deleted")
+    }
 
-//     }
-// }
+    }
+}
 
 
 //  Google Sign In 
