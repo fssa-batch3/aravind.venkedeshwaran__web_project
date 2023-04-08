d8d7839991d4c911e83c3ea76551d2be1d5f0dc7
aravindramvjs <aravindramvj@gmail.com>
2023-04-08 13:52:56 +0530

    feat: adding google sign in

--- assets/JS/profile.js
+++ assets/JS/profile.js
@@ -1,12 +1,12 @@
-// let users = JSON.parse(localStorage.getItem('userdata'));
-// let userlogin = JSON.parse(localStorage.getItem('userlogin'));
+let users = JSON.parse(localStorage.getItem('userdata'));
+let userlogin = JSON.parse(localStorage.getItem('userlogin'));
 
-// for (let i = 0; i <  users.length; i++) {
-//     if(userlogin == users[i].email  ||userlogin == users[i].name ){
-//     document.getElementById("name").value = users[i]["name"];
-//     document.getElementById("email").value = users[i]["email"];
-//     }
-// }
+for (let i = 0; i <  users.length; i++) {
+    if(userlogin == users[i].email  ||userlogin == users[i].name ){
+    document.getElementById("name").value = users[i]["name"];
+    document.getElementById("email").value = users[i]["email"];
+    }
+}
 //  enable and disable input for edit and update operation
 function enablename() {
 document.getElementById("name").disabled = false;
@@ -51,21 +51,4 @@ function deleteprofile() {
     }
 
     }
-}
-
-
-//  Google Sign In 
-function onSignIn(googleUser) {
-    let profile = googleUser.getBasicProfile();
-    $("#name").text(profile.getName());
-    $("#email").text(profile.getEmail());
-    $("#profilePic").attr('src',profile.getImageUrl());
-}
-
-//  Google Sign Out
-function signOut() {
-    let auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      alert('User signed out.');
-    });
 }
