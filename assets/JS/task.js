5a6ca061cccc6f7d3de0b51804608af2047b3d57
aravindramvjs <aravindramvj@gmail.com>
2023-04-22 08:41:09 +0530

    feat: display taskName in taskDetails

--- assets/JS/task.js
+++ assets/JS/task.js
@@ -16,82 +16,42 @@ date.innerText = currentDate.slice(8,10);
 month.innerText = currentDate.slice(4,7);
 
 
-// Digital Clock
-let digitaltime = document.getElementById("digitaltime")
-let thishour = displayClock().slice(16,21);
-window.onload = displayClock();
-function displayClock(){
-    let display = Date();
-    document.querySelector("#digitaltime").innerHTML = display.slice(16,25);
-    setTimeout(displayClock, 1000); 
-    return display
-}
-// digitaltime.style.display = "none";
-
-
-
 
 // add and save task in local storage
 let existingtask = JSON.parse(localStorage.getItem("usertasks")) ?? [];
-
+existingtask.forEach(task => rendertask(task))
 
 // add task function
 function addtask() {
     let newtask = { 
         taskId: Math.floor(Math.random() * Date.now()),
-        todos: taskinput.value,
-        date: Date().slice(4,16),
-        time: Date().slice(16, 25),
-        status: "not completed",
-        loc: "myday"
+        taskname: taskinput.value,
+        createddate: Date().slice(4,16),
+        createdtime: Date().slice(16, 25),
+        duedate : "",
+        duetime : "",
+        priority: "",
+        notes : "",
+        subtask : "",
+        attachment : "",
+        status: "not completed"
     };
     existingtask.push(newtask);
     localStorage.setItem("usertasks", JSON.stringify(existingtask));
     Notify.success(`${taskinput.value} Task Added`);
     rendertask(newtask);
-    tasksuggestion(newtask);
     taskinput.value = ""
-    // let j = 0;
-    // for(let i = 0; i < existingtask.length; i++){
-    //     if(existingtask[i]["loc"] == "myday")
-    //         j = 1;
-    //     else if(existingtask[i]["loc"] == "suggestion"){
-    //         j = 0;
-    //     }
-    // }
-    // if(j == 1){
-        
-    //     taskinput.value = ""
-    // }
-    // else{
-    //     tasksuggestion(newtask)
-    //     taskinput.value = ""
-    // }
-    
 }
 
-let j = 0;
-for(let i = 0; i < existingtask.length; i++){
-    if(existingtask[i]["loc"] == "myday")
-        j = 1;
-    else if(existingtask[i]["loc"] == "suggestion"){
-        j = 0;
-    }
-}
-if(j == 1){
-    existingtask.forEach(task => rendertask(task))
-}
-else{
-    existingtask.forEach(task => tasksuggestion(task))
-}
 
 
 // render task in local storage function
 function rendertask(task) {
     let li = document.createElement('li')
-    let indexStatus = existingtask.findIndex(t => t.todos == task.todos)
+    let indexStatus = existingtask.findIndex(t => t.taskname == task.taskname)
     if(existingtask[indexStatus]["status"] == "completed"){
-    li.innerHTML = `<span id="taskspan" onclick="popup()"><s>${task.todos}</s>
+    li.innerHTML = `<span id="taskspan" onclick="popup()">
+                    <span id="taskHeading"> <s>${task.taskname}</s></span>
                     <span id="todosdescrip">Project Description</span>
                     </span>
                     <button id="checkmark" >&#10004;</button>
@@ -99,30 +59,33 @@ function rendertask(task) {
                     `
     }
     else{
-        li.innerHTML = `<span id="taskspan" onclick="popup()">${task.todos}
-                    <span id="todosdescrip">Project Description</span>
-                    </span>
+        li.innerHTML = `<span id="taskspan" onclick="popup()">
+                        <span id="taskHeading">${task.taskname}</span>
+                        <span id="todosdescrip">Project Description</span>
+                        </span>
+                    
                     <button id="checkmark" >&#10004;</button>
                     <button id="deletebtn"><i class='fas fa-trash' style='color:white'></i></button>
                     `
     }
     li.querySelector('#checkmark').addEventListener('click', () => {
-        let indexDone = existingtask.findIndex(t => t.todos == task.todos)
-        li.innerHTML = `<span id="taskspan" onclick="popup()"><s>${task.todos}</s>
+        let indexDone = existingtask.findIndex(t => t.taskname == task.taskname)
+        li.innerHTML = `<span id="taskspan" onclick="popup()">
+                        <span id="taskHeading"> <s>${task.taskname}</s></span>
                         <span id="todosdescrip">Project Description</span>
                         </span>
                         <button id="checkmark" >&#10004;</button>
                         <button id="deletebtn"><i class='fas fa-trash' style='color:white'></i></button>`
         existingtask[indexDone]["status"] = "completed"
         localStorage.setItem("usertasks", JSON.stringify(existingtask));
-        Notify.success(`${task.todos} Task Completed`)
+        Notify.success(`${task.taskname} Task Completed`)
         
     });
     li.querySelector('#deletebtn').addEventListener('click', () => {
-        let indexDel = existingtask.findIndex(t => t.todos == task.todos)
+        let indexDel = existingtask.findIndex(t => t.taskname == task.taskname)
         existingtask.splice(indexDel, 1)
         localStorage.setItem("usertasks", JSON.stringify(existingtask));
-        Notify.error(`${task.todos} Task Removed`)
+        Notify.error(`${task.taskname} Task Removed`)
         li.remove()
         
     });
@@ -131,24 +94,24 @@ function rendertask(task) {
 }
 
 
-// Task suggestion
-
-function tasksuggestion(task) {
-    if(thishour == "20:40" && task.status == "not completed" && (task.loc == "myday" || task.loc == "suggestion")){
-        let indexlocation = existingtask.findIndex(t => t.taskId == task.taskId)
-        existingtask[indexlocation]["loc"] = "suggestion";
-        let weekday = "Yesterday";
-        let li = document.createElement('li')
-        li.innerHTML = `<span id="suggestionspan"><i class="fa fa-plus"></i> ${task.todos}</span> <br> <br>
-                        <span id="sugduedate">From ${weekday}</span>
-                        `
-
-        suggestiontask.append(li)
-        tasklist.remove(li)
-        localStorage.setItem("usertasks", JSON.stringify(existingtask));
+// Task Details
+let taskTitle = document.getElementById("taskName");
+let taskspanlist = document.querySelectorAll("li");
+taskspanlist.forEach(task => {
+    task.onclick = ()=>{
+        const clickedText = task.querySelector('#taskHeading').textContent;
+        taskTitle.innerText = clickedText
     }
-}
+})
 
+// Display current time in task details
+let duetime = document.getElementById("duetime");
+let currentTime = Date().slice(16, 21);
+duetime.value = currentTime;
+
+// Display current date in task details
+let duedate = document.getElementById("duedate");
+duedate.value = new Date().toJSON().slice(0, 10)
 
 
 
@@ -182,16 +145,17 @@ taskinput.addEventListener('keypress',e =>{
 
 
 // function for pop up window
-let task_notes = document.querySelector(".task-notes")
+let taskDetails = document.querySelector(".taskDetails");
 function popup() {
-    task_notes.style.display = "block";
+    taskDetails.style.display = "block";
+    
 }
 
 
 // function for close pop up window
 let close = document.getElementById("close")
 close.onclick = function() {
-    task_notes.style.display = "none";
+    taskDetails.style.display = "none";
 }
 
 
@@ -199,37 +163,76 @@ close.onclick = function() {
 let taskPage = document.querySelector(".task-page")
 window.onclick = function (event) {
     if (event.target == taskPage) {
-        task_notes.style.display = "none";
+        taskDetails.style.display = "none";
     }
 }
 
 
 
 
+// Digital Clock
+// let digitaltime = document.getElementById("digitaltime")
+// let thishour = displayClock().slice(16,21);
+// window.onload = displayClock();
+// function displayClock(){
+//     let display = Date();
+//     document.querySelector("#digitaltime").innerHTML = display.slice(16,25);
+//     setTimeout(displayClock, 1000); 
+//     return display
+// }
+// digitaltime.style.display = "none";
 
-let arrowicon = document.getElementById("arrow");
-let bulbicon = document.getElementById("bulb");
-let suggestions = document.querySelector(".suggestiontask")
 
-arrowicon.style.display = "none";
-bulbicon.style.display = "block";
-suggestions.style.display = "none"
+// Task suggestion
 
-bulbicon.onclick = ()=>{
-    arrowicon.style.display = "block";
-    bulbicon.style.display = "none";
-    suggestions.style.display = "block"
+// function tasksuggestion(task) {
+//     if(thishour == "20:40" && task.status == "not completed" && (task.loc == "myday" || task.loc == "suggestion")){
+//         let indexlocation = existingtask.findIndex(t => t.taskId == task.taskId)
+//         existingtask[indexlocation]["loc"] = "suggestion";
+//         let weekday = "Yesterday";
+//         let li = document.createElement('li')
+//         li.innerHTML = `<span id="suggestionspan"><i class="fa fa-plus"></i> ${task.todos}</span> <br> <br>
+//                         <span id="sugduedate">From ${weekday}</span>
+//                         `
+
+//         suggestiontask.append(li)
+//         tasklist.remove(li)
+//         localStorage.setItem("usertasks", JSON.stringify(existingtask));
+//     }
+// }
+
+// let j = 0;
+// for(let i = 0; i < existingtask.length; i++){
+//     if(existingtask[i]["loc"] == "myday")
+//         j = 1;
+//     else if(existingtask[i]["loc"] == "suggestion"){
+//         j = 0;
+//     }
+// }
+// if(j == 1){
+//     existingtask.forEach(task => rendertask(task))
+// }
+// else{
+//     existingtask.forEach(task => tasksuggestion(task))
+// }
+
+
+// let arrowicon = document.getElementById("arrow");
+// let bulbicon = document.getElementById("bulb");
+// let suggestions = document.querySelector(".suggestiontask")
+
+// arrowicon.style.display = "none";
+// bulbicon.style.display = "block";
+// suggestions.style.display = "none"
+
+// bulbicon.onclick = ()=>{
+//     arrowicon.style.display = "block";
+//     bulbicon.style.display = "none";
+//     suggestions.style.display = "block"
     
-}
-arrowicon.onclick = ()=>{
-    arrowicon.style.display = "none";
-    bulbicon.style.display = "block";
-    suggestions.style.display = "none"
-}
-
-
-
-
-
-
-
+// }
+// arrowicon.onclick = ()=>{
+//     arrowicon.style.display = "none";
+//     bulbicon.style.display = "block";
+//     suggestions.style.display = "none"
+// }
