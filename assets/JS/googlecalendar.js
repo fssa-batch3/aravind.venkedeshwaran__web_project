47205ec2b43ba6e6664150689045b500bb11b6fc
aravindramvjs <aravindramvj@gmail.com>
2023-05-21 15:53:30 +0530

    fix: google calendar

--- assets/JS/googlecalendar.js
+++ assets/JS/googlecalendar.js
@@ -1,66 +1,50 @@
-// Load the Google Calendar API
-function loadCalendarAPI() {
-    gapi.load('client', { callback: initClient });
+// Load the Google Calendar API asynchronously
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
   }
   
-  // Initialize Google API client library and set up the API credentials
+  // Initialize the Google Calendar API client
   function initClient() {
     gapi.client.init({
-      apiKey: 'AIzaSyB-KNY4pNja2u2dh317PINNA2i0ZhJnJLY', // Replace with your Google API key
-      clientId: '879687241176-kh9udu4dc6at9jqlmfohlrs0ovi01pms.apps.googleusercontent.com', // Replace with your own client ID
+      apiKey: 'YOUR_API_KEY',
+      clientId: 'YOUR_CLIENT_ID',
       discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
-      scope: 'https://www.googleapis.com/auth/calendar'
-    }).then(function() {
-      console.log('Google API client initialized successfully!');
-      // Enable the form submission once the API is initialized
-      document.getElementById('eventForm').addEventListener('submit', handleFormSubmission);
-    }, function(error) {
-      console.error('Error initializing Google API client:', error);
+      scope: 'https://www.googleapis.com/auth/calendar.events'
+    }).then(() => {
+      // Add event listener to the form
+      document.getElementById('event-form').addEventListener('submit', createEvent);
     });
   }
   
-  // Function to handle form submission
-  function handleFormSubmission(submissionEvent) {
-    submissionEvent.preventDefault(); // Prevent form from submitting normally
+  // Create a new Google Calendar event
+  function createEvent(event) {
+    event.preventDefault();
   
-    // Get form values
-    const eventName = document.getElementById('eventName').value;
-    const eventDate = document.getElementById('eventDate').value;
-    const eventTime = document.getElementById('eventTime').value;
+    const title = document.getElementById('event-title').value;
+    const date = document.getElementById('event-date').value;
+    const time = document.getElementById('event-time').value;
   
-    // Create an event object
-    const event = {
-      summary: eventName,
+    const dateTime = new Date(`${date}T${time}:00`).toISOString();
+  
+    const eventDetails = {
+      summary: title,
       start: {
-        dateTime: `${eventDate}T${eventTime}:00`,
-        timeZone: 'Asia/Kolkata' // Replace with your desired time zone
+        dateTime: dateTime
       },
       end: {
-        dateTime: `${eventDate}T${eventTime}:00`,
-        timeZone: 'Asia/Kolkata' // Replace with your desired time zone
+        dateTime: dateTime
       }
     };
   
-    // Call the function to add the event to Google Calendar
-    addEventToGoogleCalendar(event);
-  }
-  
-  // Function to add an event to Google Calendar
-  function addEventToGoogleCalendar(event) {
     gapi.client.calendar.events.insert({
       calendarId: 'primary',
-      resource: event
-    }).then(function(response) {
-      console.log('Event created: ', response.result);
+      resource: eventDetails
+    }).then(() => {
       alert('Event created successfully!');
-      document.getElementById('eventForm').reset(); // Reset the form after successful submission
-    }, function(reason) {
-      console.error('Error creating event: ', reason.result.error.message);
+      document.getElementById('event-form').reset();
+    }, error => {
+      console.error('Error creating event:', error);
       alert('Error creating event. Please try again.');
     });
   }
-  
-  // Load Google API client library
-  gapi.load('client', loadCalendarAPI);
-
   
